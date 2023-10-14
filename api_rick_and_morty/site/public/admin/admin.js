65894ebf9f8aebe3cafde354d6e50462239aca89
NavarroT <tiago,navarro@sptech.school>
2023-10-14 18:06:33 -0300

    adicionando forma para deletar os avisos

--- api_rick_and_morty/site/public/admin/admin.js
+++ api_rick_and_morty/site/public/admin/admin.js
@@ -27,9 +27,13 @@ function exibir_avisos(data) {
                 <article>
                   <div class="article-wrapper">
                     <div class="article-body">
-                      Autor: ${data[i]["nome"]}
-                      <h2>${data[i]["titulo"]}</h2>
-                      <p>
+                      <div style="width: 100%; display: flex; justify-content: space-between;">
+                        Autor: ${data[i]["nome"]} 
+                        <img onclick="excluirAviso(this.id)" id="${data[i]["idAviso"]}" style=" cursor:pointer; height: 20px;" src="../img/icon-delete.jpg">
+                      </div>
+                      <br>
+                        <h3>${data[i]["titulo"]}</h3>
+                        <p>
                         ${data[i]["descricao"]}
                       </p>
                     </div>
@@ -37,6 +41,33 @@ function exibir_avisos(data) {
                 </article>
         `;
     }
+}
 
+function excluirAviso(idAviso) {
+    const deletarAvisoBooelan = prompt('Digite "deletar" para confirmar esta operação')
+
+    if (deletarAvisoBooelan != "deletar") {
+        window.alert("Operação cancelada")
+        return
+    }
+
+
+    var url = `http://localhost:3333/avisos/deletar/${idAviso}`;
+    var options = {
+        method: 'DELETE',
+    };
+
+    fetch(url, options)
+        .then(response => {
+            if (response.ok) {
+                window.alert('Aviso deletado com sucesso');
+                window.location = '../admin/aviso-admin.html'
+            } else {
+                throw new Error('Erro ao gravar o resultado do quiz.');
+            }
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+        });
 
 }
