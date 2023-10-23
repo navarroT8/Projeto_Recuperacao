02c7e687071af1086b2e03d8d845f45158b2d996
NavarroT <tiago,navarro@sptech.school>
2023-10-23 01:59:22 -0300

    adicionando busca das perguntas do banco e criando nova lógica para verificar se é correta

--- api_rick_and_morty/site/public/dashboard/quiz.js
+++ api_rick_and_morty/site/public/dashboard/quiz.js
@@ -3,13 +3,6 @@
 // Aqui estou fazendo uma função para iniciar o quiz e ir trocando de imagem pergutas e repostas
 
 
-function iniciar_quiz() {
-    comeco_quiz.style.display = 'none';
-    questoes.style.display = 'flex';
-
-    aparecer.innerHTML = `${lista_perguntas[i]}`;
-    img.innerHTML = `${lista_imagem[i]}`;
-}
 // aqui fiz uma função para caso ele clique no botão cadastrar na historia ele direciona para o cadastro.
 function cadastrar() {
     window.location = "cadastro.html";
